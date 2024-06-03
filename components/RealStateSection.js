b57263164ebf0a4af3a6244158aa2226260f6dcd
ammarah63 <ammarahkhaleeq@gmail.com>
2024-06-03 19:41:41 +0500

    "Refactored RealStateSection component layout and styling"

--- components/RealStateSection.js
+++ components/RealStateSection.js
@@ -50,62 +50,70 @@ const RealStateSection = () => {
             A REAL STATE AND PROPERTY PLATFORM THAT IS CHANGING THE WORLD
           </p>
         </div>
-        <div
-          className="grid grid-cols-2 gap-1 mt-14 lg:mt-12 "
-          data-aos="fade-right"
-          data-aos-once="true"
-        >
+        <div className="flex justify-center items-center">
           <div>
-            <p className="text-xs lg:text-2xl tracking-widest">
-              A REVOLUTIONARY PLATFORM
-            </p>
-            <p className="text-base lg:text-4xl mt-2 tracking-widest">
-              ENTRIES - SELLERS
-            </p>
-            <p className="text-lg  lg:text-5xl mt-2 tracking-widest">
-              WORLDWIDE
-            </p>
-          </div>
-          <div></div>
-        </div>
-        <div className="grid grid-cols-2 gap-1  !overflow-hidden">
-          <div></div>
-          <div className="ml-auto" data-aos="fade-left" data-aos-once="true">
-            <div className="flex items-center justify-center">
-              <Image
-                src={logo}
-                width={100}
-                height={100}
-                className="min-w-52 lg:min-h-20 lg:min-w-96"
-              />
+            <div
+              className=" grid grid-cols-2 gap-1 mt-14 lg:mt-12 "
+              data-aos="fade-right"
+              data-aos-once="true"
+            >
+              <div>
+                <p className="text-xs lg:text-2xl tracking-widest">
+                  A REVOLUTIONARY PLATFORM
+                </p>
+                <p className="text-base lg:text-4xl mt-2 tracking-widest">
+                  ENTRIES - SELLERS
+                </p>
+                <p className="text-lg  lg:text-5xl mt-2 tracking-widest">
+                  WORLDWIDE
+                </p>
+              </div>
+              <div></div>
             </div>
-            <p className="text-center text-xs lg:text-sm tracking-widest mt-7">
-              CONNECTING USERS FROM ACROSS THE GLOBE
-            </p>
-            <p className="text-center  text-xs lg:text-sm tracking-widest">
-              TO FACILITATE LIFE&apos;S MOST IMPORTANT
-            </p>
-            <p className="text-center  text-xs lg:text-sm tracking-widest">
-              PERSONAL TRANSACTIONS
-            </p>
+            <div className="grid grid-cols-2 gap-1  !overflow-hidden">
+              <div></div>
+              <div
+                className="ml-auto"
+                data-aos="fade-left"
+                data-aos-once="true"
+              >
+                <div className="flex items-center justify-center">
+                  <Image
+                    src={logo}
+                    width={100}
+                    height={100}
+                    className="min-w-52 lg:min-h-20 lg:min-w-96"
+                  />
+                </div>
+                <p className="text-center text-xs lg:text-sm tracking-widest mt-7">
+                  CONNECTING USERS FROM ACROSS THE GLOBE
+                </p>
+                <p className="text-center  text-xs lg:text-sm tracking-widest">
+                  TO FACILITATE LIFE&apos;S MOST IMPORTANT
+                </p>
+                <p className="text-center  text-xs lg:text-sm tracking-widest">
+                  PERSONAL TRANSACTIONS
+                </p>
+              </div>
+            </div>
+            <div className="grid grid-cols-2 gap-1 lg:-mt-10">
+              <div
+                data-aos="fade-right"
+                data-aos-once="true"
+                className="overflow-hidden"
+              >
+                <p className="text-base lg:text-3xl tracking-widest">
+                  THE BEST OF THE BEST
+                </p>
+                <p className="tracking-widest text-xs lg:text-sm mt-4 lg:mt-7">
+                  A COMBINATION OF AUTOMATION AND MANUAL CURATION OUR PRO AGENTS
+                  AND MODERATION TEAM SELECTS THE HIGHEST QUALITY LISTINGS ON
+                  THE MARKET FROM ACROSS THE WORLD.
+                </p>
+              </div>
+              <div></div>
+            </div>
           </div>
-        </div>
-        <div className="grid grid-cols-2 gap-1 lg:-mt-10">
-          <div
-            data-aos="fade-right"
-            data-aos-once="true"
-            className="overflow-hidden"
-          >
-            <p className="text-base lg:text-3xl tracking-widest">
-              THE BEST OF THE BEST
-            </p>
-            <p className="tracking-widest text-xs lg:text-sm mt-4 lg:mt-7">
-              A COMBINATION OF AUTOMATION AND MANUAL CURATION OUR PRO AGENTS AND
-              MODERATION TEAM SELECTS THE HIGHEST QUALITY LISTINGS ON THE MARKET
-              FROM ACROSS THE WORLD.
-            </p>
-          </div>
-          <div></div>
         </div>
         <div>
           <div className="flex items-center justify-center mt-7">
